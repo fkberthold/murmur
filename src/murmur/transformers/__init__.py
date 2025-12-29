038adc3a6087bcfa85913cba047f8ad1bb2ffb5b
Frank Berthold <fkberthold@gmail.com>
2025-12-29 08:56:07 -0800

    feat(transformers): register StoryDeduplicator and HistoryUpdater

--- src/murmur/transformers/__init__.py
+++ src/murmur/transformers/__init__.py
@@ -1,3 +1,5 @@
+# src/murmur/transformers/__init__.py
+from murmur.registry import TransformerRegistry
 from murmur.transformers.news_fetcher import NewsFetcher
 from murmur.transformers.brief_planner import BriefPlanner
 from murmur.transformers.script_generator import ScriptGenerator
@@ -5,11 +7,14 @@ from murmur.transformers.piper_synthesizer import PiperSynthesizer
 from murmur.transformers.story_deduplicator import StoryDeduplicator
 from murmur.transformers.history_updater import HistoryUpdater
 
-__all__ = [
-    "NewsFetcher",
-    "BriefPlanner",
-    "ScriptGenerator",
-    "PiperSynthesizer",
-    "StoryDeduplicator",
-    "HistoryUpdater",
-]
+
+def create_registry() -> TransformerRegistry:
+    """Create and populate the transformer registry."""
+    registry = TransformerRegistry()
+    registry.register(NewsFetcher)
+    registry.register(BriefPlanner)
+    registry.register(ScriptGenerator)
+    registry.register(PiperSynthesizer)
+    registry.register(StoryDeduplicator)
+    registry.register(HistoryUpdater)
+    return registry
